58010fe71af30d81fe757348eb8bb2a0c10182f9
Emmanuel <kwobapetit@gmail.com>
2020-08-05 07:57:06 +0300

    with eslint clean and watch feature added

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import numeral from 'numeral';
 
 const coursevalue = numeral(1000).formart('$0,0.00');
 
-console.log(`I would pay ${coursevalue} for this awesome course!`); //eslint-desable0line no-console
+
+//eslint-disable-next-line no-console
+console.log(`I would pay ${coursevalue} for this awesome course!`);
